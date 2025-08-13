a16878d9b329f7ea5ec79a3c3e22787ae7fbb487
emile <emilehouedakor@gmail.com>
2025-08-13 17:49:55 +0000

    third

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
 import Navbar from "./components/Navbar"
 import Loader from "./components/Loader"
+import "./App.css"
 
 function App() {
   const [user, setUser] = useState(null)
